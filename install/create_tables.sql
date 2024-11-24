0ff22431bf893bb56ba5e742c0ad05fc0b6fe118
Artem Hordiyevskyy <139376262+slonic32@users.noreply.github.com>
2024-11-25 01:03:21 +0200

    Update create_tables.sql

--- install/create_tables.sql
+++ install/create_tables.sql
@@ -6,7 +6,7 @@ CREATE TABLE Users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL UNIQUE,
-    password VARCHAR(20) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     phone VARCHAR(20),
     avatar VARCHAR(255),
     token VARCHAR(255),
